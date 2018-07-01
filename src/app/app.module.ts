740cc2e8894ecaa541473824cd896c47f6afdf14
crunchcubes <crunchcubes@gmail.com>
2018-07-01 18:13:38 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { MessageComponent } from './shared/message/message.component';
+import { NotificationComponent } from './shared/notification/notification.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    MessageComponent,
+    NotificationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent,
+    SidebarComponent,
+    MessageComponent,
+    NotificationComponent
+  ]
+})
+export class AppModule { }
