536a1a41a714eb08bbf2de8bd3e85bcc4a401260
crunchcubes <crunchcubes@gmail.com>
2018-07-18 23:18:27 -0500

    Updated create task component

--- src/app/modules/task/task-new/task-new.component.ts
+++ src/app/modules/task/task-new/task-new.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-task-new',
   templateUrl: './task-new.component.html',
   styleUrls: ['./task-new.component.css']
 })
 export class TaskNewComponent implements OnInit {
-
+  taskNewControl = new FormControl();
   constructor() { }
 
   ngOnInit() {
   }
 
+  addTask() {
+    const taskName = this.taskNewControl.value;
+    alert(taskName);
+  }
 }
