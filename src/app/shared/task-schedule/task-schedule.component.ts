4c3c8cc61fce4624a8b5c068e0c1fcba05fce40a
crunchcubes <crunchcubes@gmail.com>
2018-08-22 21:57:33 -0500

    Added momentjs to .angular-cli for optimal loading

--- src/app/shared/task-schedule/task-schedule.component.ts
+++ src/app/shared/task-schedule/task-schedule.component.ts
@@ -4,15 +4,14 @@ import {ScriptService} from '../../core/services/script.service'
 
 @Component({
   selector: 'app-task-schedule',
-  templateUrl: './task-schedule.component.html',
-  //styleUrls: ['./fullcalendar.min.css']
+  templateUrl: './task-schedule.component.html'
 })
 export class TaskScheduleComponent implements OnInit {
 
   constructor(private script: ScriptService) { }
 
   ngOnInit() {
-    this.script.load('momentjs', 'fullcalendar', ).then(data => {
+    this.script.load(/*'momentjs', */'fullcalendar', ).then(data => {
       console.log('script loaded ', data);
       initializeCalendar();
   }).catch(error => console.log(error));
